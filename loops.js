db6d34bb99f8a0784f7d12c2dbbb620d1a7b343d
pshivamm <shivam.pandey63079801@gmail.com>
2026-04-27 12:30:58 +0530

    loops completed

--- loops.js
+++ loops.js
@@ -1,2 +1,16 @@
 //loops in javascript
 // for loop used ot perform repetitive actions.
+//statement:
+
+//for loop:
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i)
+// }
+
+let n = prompt("Enter the value of n: ")
+let sum = 0;
+for (let i = 0; i < n; i++){
+    sum += (i+1);
+    console.log(i+1, sum);
+}
+console.log("Sum of the first", n, "natural number is", sum);
