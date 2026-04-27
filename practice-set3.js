b7f155afba8afafdc347859e709e71e4d43b4622
pshivamm <shivam.pandey63079801@gmail.com>
2026-04-27 17:55:58 +0530

    practiceset 3 and strings

---
+++ practice-set3.js
@@ -0,0 +1,50 @@
+// Chapter 2 - Practice set
+
+// Write a program to print the marks of a student in object using for loop.
+// obj = { harry: 98, vohan: 70, aakash: 73}
+
+let marks = {
+    harry: 98, 
+    vohan: 70, 
+    aakash: 73
+}
+
+// for (let i in marks){
+//     console.log("marks of the " + i + " is " + marks[i]);
+// }
+
+//another way
+for (let i = 0; i < Object.keys(marks).length; i++){
+    console.log("makrks of the " + Object.keys(marks));
+    console.log("makrks of the " + Object.keys(marks)[i]);
+    console.log("makrks of the " + Object.keys(marks)[i] + " is " + marks[Object.keys(marks)[i]]);
+}
+
+
+// Write the program in Q1. using for in loop.
+
+for (let i in marks){
+    console.log("marks of the " + i + " is " + marks[i]);
+}
+
+// Write a program to print "try again" until the user enters the correct number.
+
+let correctNumber = 40;
+let userInput;
+
+while (userInput !== correctNumber) {
+    userInput = prompt("Enter the correct number: ");
+    userInput = Number.parseInt(userInput);
+    console.log(userInput + " try again");
+}
+
+console.log("You have entered the correct number:" + userInput);
+
+
+// Write a function to find mean of 5 numbers.
+const mean = (a, b, c ,d, e) => (a + b + c + d + e)/5;
+
+let meanResult = mean(15, 45, 53, 44, 75);
+
+// meanResult = Math.round(meanResult);
+console.log(meanResult);
---
+++ strings.js
@@ -0,0 +1,17 @@
+let a = "shivam";
+console.log(a);
+
+let b = 'hello world';
+console.log(b);
+
+//template literals: it is a new feature in ES6 which allows us to use multi-line strings and string interpolation.
+let boy1 = "john";
+let boy2 = "samay";
+
+console.log(`${boy1} is the best friend of ${boy2}
+    and ${boy2} is the best friend of ${boy1}`);
+
+//Escape sequence characters: it is used to print the special characters in the string.
+// \!, \", \', \\, \n, \t, \r, \b, \f, \v, \0
+let c = "hello \0world";
+console.log(c);
